50dcde7b64ec9f2aa769e8ee483bd0dfc3f24e18
Tomislav Skoda <tskoda23@gmail.com>
2023-07-19 12:24:49 +0200

    Map brigthness to ascii

--- convert_to_ascii.py
+++ convert_to_ascii.py
@@ -1,10 +1,37 @@
 from PIL import Image
 
+_ASCII_ARR = '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+
+razmak = 256 / len(_ASCII_ARR)
+_ASCII_MAP = {}
+
+j = 1
+
+for i in range(0, 256):
+    if i > razmak * j:
+        j = j + 1
+    _ASCII_MAP[i] = j
+
+
+def avg_brightness(px_tuple):
+    return int((px_tuple[0] + px_tuple[1] + px_tuple[2]) / 3)
+
+
+def map_brightness_to_ascii(brightness):
+    return _ASCII_ARR[_ASCII_MAP[brightness]]
+
+
 im = Image.open("ascii-pineapple.jpg")
-print("Image loaded with widht {0}px and height {1}px\n".format(im.width, im.height))
+print("Image loaded with width {0}px and height {1}px\n".format(im.width, im.height))
 
 
-pixel_data = list(im.getdata())
+px_data = list(im.getdata())
+px_brightness_list = []
+for px_tuple in px_data:
+    px_brightness_list.append(avg_brightness(px_tuple))
 
-print("This is pixel data for this image\n")
-print(pixel_data)
+ascii_list = ""
+for px_b in px_brightness_list:
+    ascii_list = ascii_list + map_brightness_to_ascii(px_b)
+
+print(ascii_list)
