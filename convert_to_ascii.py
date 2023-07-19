90e460a428e58996f25185730451f92cee8055f8
Tomislav Skoda <tskoda23@gmail.com>
2023-07-19 11:27:05 +0200

    Get pixel data

--- convert_to_ascii.py
+++ convert_to_ascii.py
@@ -1,4 +1,10 @@
 from PIL import Image
 
 im = Image.open("ascii-pineapple.jpg")
-print("Image loaded with widht {0}px and height {1}px".format(im.width, im.height))
+print("Image loaded with widht {0}px and height {1}px\n".format(im.width, im.height))
+
+
+pixel_data = list(im.getdata())
+
+print("This is pixel data for this image\n")
+print(pixel_data)
