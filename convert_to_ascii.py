b741ae155afc24d042d4afd1b873acf0e201aad1
Tomislav Skoda <tskoda23@gmail.com>
2023-07-21 15:16:27 +0200

    Add color and options for min max and luminance

--- convert_to_ascii.py
+++ convert_to_ascii.py
@@ -1,50 +1,88 @@
+#!/usr/bin/env python3
 from PIL import Image
 import numpy
 import sys
 
-_SCALE = int(sys.argv[1])
+#GLOBALS
 
 _ASCII_ARR = '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
-
-razmak = 256 / len(_ASCII_ARR)
 _ASCII_MAP = {}
+_OKGREEN = '\033[92m'
 
-j = 1
+def init():
+    global _ASCII_ARR
+    global _ASCII_MAP
 
-for i in range(0, 256):
-    if i > razmak * j:
-        j = j + 1
-    _ASCII_MAP[i] = j
+    space = 256 / len(_ASCII_ARR)
 
+    j = 1
 
-def avg_brightness(px_tuple):
-    brightness = int((px_tuple[0] + px_tuple[1] + px_tuple[2]) / 3)
-    return brightness
+    for i in range(0, 256):
+        if i > space * j:
+            j = j + 1
+        _ASCII_MAP[i] = j
+
+def min_max_b(px_tuple):
+    return int((max(px_tuple[0], px_tuple[1], px_tuple[2]) + min(px_tuple[0], px_tuple[1], px_tuple[2])) / 2)
+
+def lum_b(px_tuple):
+    return int(numpy.sqrt( 0.299*numpy.square(px_tuple[0]) + 0.587*numpy.square(px_tuple[1]) + 0.114*numpy.square(px_tuple[2])))
+
+def avg_b(px_tuple):
+    return int((px_tuple[0] + px_tuple[1] + px_tuple[2]) / 3)
+
+def get_brightness(px_tuple, b_type):
+    if b_type == 0:
+        return(avg_b(px_tuple))
+    elif b_type == 1:
+        return(min_max_b(px_tuple))
+    elif b_type == 2:
+        return(lum_b(px_tuple))
+    else:
+        return(avg_b(px_tuple))
 
 
 def map_brightness_to_ascii(brightness):
+    global _ASCII_ARR
+    global _ASCII_MAP
+
     return _ASCII_ARR[_ASCII_MAP[brightness]]
 
+def convert_to_ascii(scale = 10, b_type = 0, image = "ascii-pineapple.jpg"):
+    big_im = Image.open(image)
 
-big_im = Image.open("ascii-pineapple.jpg")
+    small_w = round(big_im.width / scale)
+    small_h = round(big_im.height / scale)
+    im = big_im.resize((small_w, small_h))
 
-small_w = round(big_im.width / _SCALE)
-small_h = round(big_im.height / _SCALE)
-im = big_im.resize((small_w, small_h))
+    all_px_data = im.getdata()
+    b_data = []
+    for px in all_px_data:
+        b_data.append(get_brightness(px, b_type))
 
-all_px_data = im.getdata()
-b_data = []
-for px in all_px_data:
-    b_data.append(avg_brightness(px))
+    px_matrix = numpy.reshape(b_data, (im.height, im.width))
 
-px_matrix = numpy.reshape(b_data, (im.height, im.width))
+    painting = ""
+    for row in px_matrix:
+        line = ""
+        for col in row:
+            ascii = map_brightness_to_ascii(col)
+            line = line + ascii + ascii + ascii
+        painting = painting + line + "\n"
 
-painting = ""
-for row in px_matrix:
-    line = ""
-    for col in row:
-        ascii = map_brightness_to_ascii(col)
-        line = line + ascii + ascii + ascii
-    painting = painting + line + "\n"
+    return painting
 
-print(painting)
+init()
+ascii_art = ""
+
+if len(sys.argv) > 3:
+    ascii_art = convert_to_ascii(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
+elif len(sys.argv) > 2:
+    ascii_art = convert_to_ascii(int(sys.argv[1]), int(sys.argv[2]))
+elif len(sys.argv) > 1:
+    ascii_art = convert_to_ascii(int(sys.argv[1]))
+else:
+    ascii_art = convert_to_ascii()
+
+print(_OKGREEN + ascii_art)
+
