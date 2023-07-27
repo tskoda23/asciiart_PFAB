a4e451126e8bb320467bc44ca46f2ab1d3ed343d
Tomislav Skoda <tskoda23@gmail.com>
2023-07-27 22:37:16 +0200

    Add invert option

--- convert_to_ascii.py
+++ convert_to_ascii.py
@@ -6,6 +6,7 @@ import sys
 # GLOBALS
 
 _ASCII_ARR = '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+_ASCII_ARR_INV = _ASCII_ARR[::-1]
 _ASCII_MAP = {}
 _OKGREEN = "\033[92m"
 
@@ -24,7 +25,6 @@ def init():
         _ASCII_MAP[i] = j
 
 
-#
 def min_max_b(px_tuple):
     return int(
         (
@@ -60,14 +60,18 @@ def get_brightness(px_tuple, b_type):
         return avg_b(px_tuple)
 
 
-def map_brightness_to_ascii(brightness):
-    global _ASCII_ARR
+def map_brightness_to_ascii(brightness, invert):
     global _ASCII_MAP
 
-    return _ASCII_ARR[_ASCII_MAP[brightness]]
+    if invert == 1:
+        global _ASCII_ARR_INV
+        return _ASCII_ARR_INV[_ASCII_MAP[brightness]]
+    else:
+        global _ASCII_ARR
+        return _ASCII_ARR[_ASCII_MAP[brightness]]
 
 
-def convert_to_ascii(scale=10, b_type=0, image="ascii-pineapple.jpg"):
+def convert_to_ascii(scale=10, b_type=0, invert=0, image="ascii-pineapple.jpg"):
     big_im = Image.open(image)
 
     small_w = round(big_im.width / scale)
@@ -85,7 +89,7 @@ def convert_to_ascii(scale=10, b_type=0, image="ascii-pineapple.jpg"):
     for row in px_matrix:
         line = ""
         for col in row:
-            ascii = map_brightness_to_ascii(col)
+            ascii = map_brightness_to_ascii(col, invert)
             line = line + ascii + ascii + ascii
         painting = painting + line + "\n"
 
@@ -95,8 +99,15 @@ def convert_to_ascii(scale=10, b_type=0, image="ascii-pineapple.jpg"):
 if __name__ == "__main__":
     init()
     ascii_art = ""
-    if len(sys.argv) > 3:
-        ascii_art = convert_to_ascii(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
+    if len(sys.argv) > 4:
+        print(sys.argv[4])
+        ascii_art = convert_to_ascii(
+            int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), sys.argv[4]
+        )
+    elif len(sys.argv) > 3:
+        ascii_art = convert_to_ascii(
+            int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
+        )
     elif len(sys.argv) > 2:
         ascii_art = convert_to_ascii(int(sys.argv[1]), int(sys.argv[2]))
     elif len(sys.argv) > 1:
